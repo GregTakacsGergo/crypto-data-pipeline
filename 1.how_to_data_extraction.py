d60c716fce1940941c30fb9daa9b981b8f60de46
Greg Takács <63113441+GregTakacsGergo@users.noreply.github.com>
2023-06-18 13:44:09 +0200

    Update and rename 1.how_to_data_extraction to 1.how_to_data_extraction.py

---
+++ 1.how_to_data_extraction.py
@@ -0,0 +1,18 @@
+''' 
+After some digging I thought that I will start by first just trying to extract some basic data from an endpoint (randomly, I chose 
+coindesk API, just probably for the sake of simplicity... or so I thought (:666:)  )
+'''
+
+import requests
+import time
+
+duration = 61 
+start_time = time.time()
+
+while time.time() - start_time < duration:
+	response = requests.get("https://api.coindesk.com/v1/bpi/currentprice/BTC.json")
+	data = response.json()
+	btc_price = data["bpi"]["USD"]["rate"]
+	btc_time = data["time"]["updated"]
+	print(f"BTC price (USD) at {btc_time} is:{btc_price}")
+	time.sleep(1)
